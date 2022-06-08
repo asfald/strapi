b581b3d2b4fe43efbfe0025929fbe9775dd27703
asfald <rain.oselin@tptlive.ee>
2022-06-08 20:14:29 +0300

    keys

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     app: {
-        keys: env.array(['OEsKeJyv1hdrGNFkqtK4Ow', 'CWSURyQqQ0EEj+f3+F/dXQ'], ['eEOI9IZVmZDIKYzb7JEOkw', 'PJgSU8lSe0+JWUk4/dsW3g']),
+        keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
     },
 });
