d1b88f8096d594de27302690093cc087f6597874
asfald <rain.oselin@tptlive.ee>
2022-06-08 16:40:33 +0300

    insert Message herebacke

---
+++ src/api/workitem/controllers/workitem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  workitem controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::workitem.workitem');
---
+++ src/api/workitem/routes/workitem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * workitem router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::workitem.workitem');
