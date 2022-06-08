f4ef677d6032a3bdd26e51bbfdad6e73925b7866
asfald <rain.oselin@tptlive.ee>
2022-06-08 20:41:17 +0300

    make it better

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
+    auth: {
+        secret: env('IzPoSoc+mxpsw5OdGas9tSEXFZvmx27ojcw++TaPb2SuFm8lPy9RtrMu51w9bmSUJz9m1c0QfpZKWps72baVapnRFY988euMcYrD6xkrw/cLDoRD2095P0ZQ1IR+ud/K4n95JJI3q/vN4BJVQf8GGJdqKq7sXnJwjzsSd7nxL538XX3j0vjylPYaTuMiL317DNHIdCwMNWH3UJFZjd6RFN4uyARufisNC+3ga8i6efmoqxC90vJTfIUMraDiDOtf5mGgVsZzAjKgVGLwGtz/tVbFupH6GIdISz7bgXCfiT5P/fLIg4iF808ZD54KDlMuDhnb1DlqsWzbCPS1s+8F1A=='),
+    },
+    apiToken: {
+        salt: env('FpWyI/zeh/NyaWN7El3/4xLAJ+BhfVPk4Fj/pRbtRgD/gUOEF1MK7cxUM/QOOHQETUO47TKRj0hZ78g9rj94tOG/5Nf05dt25VoLFPpawaXsbf5jkzNSgwF/UMfwpyw1kTOe1NaplZn/BldFNxsJcfa9K+PBavCoJ5V6Zo0vAPPqAQwfAspavbzqUpLoSiq2cXYHSaanKLeKj6w0+zUeyepXv9JjbJ93DR9OMJB+rq5Dy7LnodmcK9pm8AYYj0dQEKHuYhp6vqQhWjejQNRpmr3mFVJxCIgLcraHxRvrQyJIiuvxyYsElYLvktr0CIJgW1oAJEnLS6LszC8C0l/2fw=='),
+    },
 });
